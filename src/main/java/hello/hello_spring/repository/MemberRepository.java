ee9044c217b424c28aced13d2a695a561beaa021
lee0502 <i63774071@gmail.com>
2024-09-11 05:45:19 +0900

    "feat:h2 연결"

--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
+    Member save(Member member) ;
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();

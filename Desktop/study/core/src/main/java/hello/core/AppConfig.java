25122c7cbb9a4ec8abb09a211500221f9dfb7565
lee0502 <i63774071@gmail.com>
2024-10-01 13:12:58 +0900

    ...

--- Desktop/study/core/src/main/java/hello/core/AppConfig.java
+++ Desktop/study/core/src/main/java/hello/core/AppConfig.java
@@ -6,9 +6,13 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Configuration;
 
 //나의 애플리케이션을 설정하고 구성 합니다.
+@Configuration
 public class AppConfig {
+
+
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
         //memberServiceImpl을 만들고 memberServiceImpl은 memoryMemberRepository를 사용할 것이라고 주입해주는 것
